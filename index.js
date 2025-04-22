f5ee5da4b5e101ea97f176befb6b3e34306ac7d3
snagapu <reachshrikanth@gmail.com>
2025-04-22 14:11:22 +0530

    Update index.js - corrected Configuration Error

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const { Configuration, OpenAIApi } = require ('openai');
+const OpenAI=require('openai');
+
 require('dotenv').config();
 
 
